0ee30fb280e0f40a0cfff61ee55b8951059fee4a
David Su <david.d.su@gmail.com>
2015-10-12 12:10:03 -0400

    populate grid with demo data

--- js/src/mesing.js
+++ js/src/mesing.js
@@ -3,13 +3,24 @@ var meSing = meSing || {};
 meSing.defaults = {
     steps: "1e&a2e&a3e&a4e&a",
     numMeasures: 4,
-    bpm: 120
+    bpm: 120,
+    textinput: ["Row","","","","row","","","","row","","","your","boat","","","",
+                "gent-","","","-ly","down","","","the","stream","","","","","","","",
+                "Merr-","","-il-","-ly","Merr-","","-il-","-ly","Merr-","","-il-","-ly","Merr-","","-il-","-ly",
+                "life","","","is","but","","","a","dream","","dream","","dream","","dream","",           
+               ],
+    midinoteinput: [60,"","","",60,"","","",60,"","",62,64,"","","",
+                64,"","",62,64,"","",65,67,67,67,67,"","","","",
+                72,"",72,72,67,"",67,67,64,"",64,64,60,"",60,60,
+                67,"","",65,64,"","",62,60,"",64,"",67,"",72,"",
+               ]
 };
 
 meSing.Session = function() {
     this.ctx = new AudioContext();
     this.voices = [];
     this.grid = $("#msDisplay"); //todo: else create element
+
 
     meSpeak.loadConfig("/js/lib/mespeak/mespeak_config.json");
     meSpeak.loadVoice("/js/lib/mespeak/voices/en/en-us.json", function() {
@@ -47,6 +58,8 @@ meSing.Session.prototype = {
         var steps = meSing.defaults.steps;
         var numMeasures = meSing.defaults.numMeasures;
         var widthScale = 90; // i.e. scale to x%
+        var textinput = meSing.defaults.textinput;
+        var midinoteinput = meSing.defaults.midinoteinput;
 
         for (var i=0; i<steps.length; i++) {
             var col = $("<div class='col-a'><strong>" + steps[i] + "</strong></div>");
@@ -56,8 +69,9 @@ meSing.Session.prototype = {
         for (var i=0; i<numMeasures; i++) {
             this.grid.append($("<br>"));
             for (var j=0; j<steps.length; j++) {
+                var inputIdx = (i*steps.length) + j;
                 var id = "measure"+i+"step"+j;
-                var col = $("<div class='col-a' id='"+id+"'><input class='textinput' type='text' value='text'/><br><input class='freqinput' type='text' value='freq'/></div>");
+                var col = $("<div class='col-a' id='"+id+"'><input class='textinput' type='text' value='" + textinput[inputIdx] + "'/><br><input class='midinoteinput' type='text' value='" + midinoteinput[inputIdx] + "'/></div>");
                 col.css("width", (widthScale/steps.length) + "%");
                 this.grid.append(col);
             }
